e915af6cd454f48637583a6e96907c9af7311401
SebatianCB-BCV <papiomnis@pm.me>
2023-10-25 10:57:01 +0200

    IAccount and changes to user and librarian

---
+++ Domain/Models/Librarian.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public class Librarian : IAccount
+{
+    public string UUID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
--- Domain/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain;
-
-public class User
-{
-    public int Id { get; set; }
-    public string UserName { get; set; }
-    
-
-}
