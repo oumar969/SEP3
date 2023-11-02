80f497f1f52a9c94fcc0d42996a5d665c8468de2
SebatianCB-BCV <scb@blackcapitalventures.com>
2023-11-02 23:53:52 +0100

    ORM Java Server Setup

--- SEP3JavaPersistence/src/main/java/com/via/sep3javapersistence/domain/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.via.sep3javapersistence.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Integer> {
-
-}
