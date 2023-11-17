06da32fc94407a4c37f9d1a4c404945329b243fa
oumar969 <oammar198@gmail.com>
2023-11-17 18:27:21 +0100

    css stuff

--- SEP3CSharp/WebAPI/Services/AuthService.cs
+++ SEP3CSharp/WebAPI/Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿// using System.ComponentModel.DataAnnotations;
+// using System.Security.Claims;
 // using System.Text.Json;
 // using Domain.Models;
 // using FileData;
@@ -9,12 +10,22 @@
 //
 // public class AuthService : IAuthService
 // {
-//     private FileContext file = new FileContext();
+//     //private FileContext file = new FileContext();
 //     private readonly IList<User> Users = new List<User>();
 //
 //     public AuthService()
 //     {
-//         file.LoadData();
+//     //    file.LoadData();
+//     }
+//
+//     public Task LoginAsync(string username, string password)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public Task LogoutAsync()
+//     {
+//         throw new NotImplementedException();
 //     }
 //
 //     public Task<User> ValidateUser(string username, string password)
@@ -26,30 +37,31 @@
 //
 //      
 //         Console.WriteLine(username+password);
-//         User? existingUser = file.Users.FirstOrDefault(u => 
-//             u.UserName.Equals(username, StringComparison.OrdinalIgnoreCase));
-//      
-//         
-//         if (existingUser == null)
-//         {
-//             throw new Exception("User not found");
-//         }
+//         // User? existingUser = file.Users.FirstOrDefault(u => 
+//         //     u.UserName.Equals(username, StringComparison.OrdinalIgnoreCase));
+//         //
+//         //
+//         // if (existingUser == null)
+//         // {
+//         //     throw new Exception("User not found");
+//         // }
+//         //
+//         // if (!existingUser.Password.Equals(password))
+//         // {
+//         //     throw new Exception("Password mismatch");
+//         // }
 //
-//         if (!existingUser.Password.Equals(password))
-//         {
-//             throw new Exception("Password mismatch");
-//         }
-//
-//         return Task.FromResult(existingUser);
+//         // return Task.FromResult(existingUser);
+//         throw new NotImplementedException();
 //     }
 //     
 //
 //     public Task<User> RegisterUser(User user)
 //     {
 //
-//         if (string.IsNullOrEmpty(user.UserName))
+//         if (string.IsNullOrEmpty(user.Email))
 //         {
-//             throw new ValidationException("Username cannot be null");
+//             throw new ValidationException("Email cannot be null");
 //         }
 //
 //         if (string.IsNullOrEmpty(user.Password))
@@ -59,5 +71,11 @@
 //         
 //         return (Task<User>)Task.CompletedTask;
 //     }
-//     
+//
+//     public Task<ClaimsPrincipal> GetAuthAsync()
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
 // }
--- SEP3CSharp/WebAPI/Services/IAuthService.cs
+++ SEP3CSharp/WebAPI/Services/IAuthService.cs
@@ -1,9 +1,18 @@
-﻿using Domain.Models;
+﻿using System.Security.Claims;
+using Domain.Models;
 
 namespace WebApi.Services;
 
 public interface IAuthService
 {
+    public Task LoginAsync(string username, string password);
+
+    public Task LogoutAsync();
+
+    
     Task<User> ValidateUser(string username, string password);
     Task <User>  RegisterUser(User user);
+    public Task<ClaimsPrincipal> GetAuthAsync();
+
+    public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }
 }
